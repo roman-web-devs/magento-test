69f0b8b16d5960805d5de522ad2ec19e6d1bf05f
Roman Protsenko <rprotsenko@webdevs.com>
2015-09-22 17:50:26 +0300

    add Webdevs magento Test module

---
+++ app/code/local/Webdevs/Test/Block/Test.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Webdevs_Test_Block_Test
+ *
+ *
+ */
+class Webdevs_Test_Block_Test extends Mage_Core_Block_Abstract
+{
+    protected function _toHtml()
+    {
+        return __CLASS__;
+    }
+}
---
+++ app/code/local/Webdevs/Test/Helper/Data.php
@@ -0,0 +1,4 @@
+<?php
+
+class Webdevs_Test_Helper_Data extends Mage_Core_Helper_Abstract
+{}
